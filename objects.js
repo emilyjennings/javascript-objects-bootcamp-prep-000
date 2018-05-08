64db95d0f9c0c61bd98fa9098a072fbdc357c5f5
Emily Jennings <emily.jennings@flatironschool.com>
2018-05-08 17:03:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { "Slowdive": "Alison", "My Bloody Valentine": "Sometimes"};
 
 function updatePlaylist() {
-  playlist.artistName = songTitle
+  playlist.['Phil Ochs'] = 'songTitle'
 }
 
 function removeFromPlaylist(playlist, artistName) {
